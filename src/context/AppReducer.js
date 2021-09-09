538c2147da2eb97434a84e6a6d84c5316d04d130
James <james101994@yahoo.com>
2021-09-09 16:09:55 -0700

    Update transactions to reflect balance, added actions and reducers to handle changes

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,8 +1,11 @@
 export default (state, action) => {
     switch (action.type) {
-        case '':
-            return;
+        case 'DELETE_TRANSACTION':
+            return {
+                ...state,
+                transactions: state.transactions.filter(transaction => transaction.id != action.payload)
+            }
         default:
-            return;
+            return state;
     }
 }
